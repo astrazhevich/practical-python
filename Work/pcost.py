31c01c79526bffc92b1dd84a470131efefb67749
Александр Стражевич <Alexander.Strazhevich@mediascope.net>
2020-07-29 18:18:36 +0300

    portfolio_cost as a func

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,11 +3,20 @@
 # Exercise 1.27
 # import os
 
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f)
-total = 0
-for line in f:
-    row = line.split(',')
-    total = total + int(row[1]) * float(row[2].strip())
-print(f'Total cost {total:0.2f}')
-f.close()
+def portfolio_cost(filename):
+    f = open(filename, 'rt')
+    headers = next(f)
+    total = 0
+    line_num = 1
+    for line in f:
+        row = line.split(',')
+        try: 
+            total = total + int(row[1]) * float(row[2].strip())
+        except ValueError:
+            print(f"Warning: wrong values in line {line_num}!")
+        line_num += 1
+    return(total)
+    f.close()
+
+# cost = portfolio_cost('Data/portfolio.csv')
+# print(f'Total cost {cost:0.2f}')
